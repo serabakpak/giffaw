b2d402662e43705e2eaeee2b05ed0b558facc772
Sera Bak <serabak15@gmail.com>
2016-08-23 19:05:01 -0700

    adds two ajax calls to load top gifs and search gifs in scripts/app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,51 @@
+var endpoint = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
+
+var endpoint2 = 'http://api.giphy.com/v1/gifs/search?';
+
 $(document).on("ready", function(){
+	
+	//ajax captures the json object into json variable
+	$.ajax( {
+			method: 'GET',
+			url: endpoint,
+			success: onSuccess,
+			error: onError
+		});
+
+	$('.form-inline').on('submit', function (event) {
+		event.preventDefault();
+		var formData = $(this).serialize();
+		$(".gif-gallery").empty();		
+		$.ajax( {
+			method: 'GET',
+			url: endpoint2 + $('.form-inline').serialize(),
+			success: onSubmitReqSuccess,
+			error: onError
+		});
+	})
+
+function onSuccess(json) { 
+	
+	for (var i = 0; i < json.data.length; i++) {
+    	$(".gif-gallery").append('<img src="' + json.data[i].images.fixed_height.url + '">');
+	}  
+}
+
+function onSubmitReqSuccess(json) {
+	console.log(json);
+	for (var i = 0; i < json.data.length; i++) {
+    	$(".gif-gallery").append('<img src="' + json.data[i].images.fixed_height.url + '">');
+	} 
+}
+
+function onError(xhr, status, errorThrown) {
+    alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+}
+
 
 });
+
+
